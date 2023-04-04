865ccf0b9d0435e1a632662002073a14a398bd08
Mahdi gosyan <mahdigosyanapple@gmail.com>
2023-04-04 05:26:47 +0330

    Create app.js

---
+++ app.js
@@ -0,0 +1,5 @@
+const image = document.querySelector('.image');
+const hover = document.querySelector('.hover');
+const modal = document.querySelector('.modal');
+const close = document.querySelector('.close');
+
